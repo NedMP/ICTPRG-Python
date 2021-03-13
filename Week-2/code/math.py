0391260dd51ce5e3ea9fb839a5c03184e4be24a5
Ned Perkins <nedprks@gmail.com>
2021-03-13 08:20:14 +0000

    I did a stupid

--- Week-2/code/math.py
+++ Week-2/code/math.py
@@ -6,26 +6,14 @@ total = 0
 # Define the most recent input as a string
 current = 0
 
-# Track whether the program has finished running  
-finished = 0
+# Continue looping until current is the string x
+while current != "x":
 
-# Continue looping while finished is equal to 0
-while finished == 0: 
+    # Convert current to an integer and add it to the total 
+    total = total + int(current)
 
-    # Check if the most recent input was an x
-    if current == "x":
-        
-        # Convert the current to a string and print it
-        print("Your total is: " + str(total))
+    # Take a new input and set it to current
+    current = input("Enter a number: ")
 
-        # Set finished to 1 and break the loop - this is just a switch
-        finished = 1
-    
-    # If the current input isn't x run this code
-    else:
-        
-        # Convert current to an integer and add it to the total 
-        total = total + int(current)
-
-        # Take a new input and set it to current
-        current = input("Enter a number: ")
+# Convert the current to a string and print it
+print("Your total is: " + str(total))
