b72311afb9a646f9bebf4f52334a07087db2cd3d
Ned Perkins <nedprks@gmail.com>
2021-03-16 05:33:00 +0000

    Update Week 3

---
+++ Week-3/code/age.py
@@ -0,0 +1,16 @@
+# Ask what year the user was born and save it as birthYear
+birthYear = int(input("What year were you born? "))
+
+# Convert the birth year to an age and save as age
+age = 2021 - birthYear
+
+# Print You are {age} years old. 
+print("You are " + str(age) + " years old.")
+
+# If you are under 18 years old print Go Away. Child. 
+if age < 18:
+    print("Go away. Child.")
+
+# Otherwise print Come in and have a drink.
+else: 
+    print("Come in and have a drink!")
---
+++ Week-3/code/signin.py
@@ -0,0 +1,13 @@
+# Set the correct user detail
+correctUser = "bob"
+correctPass = "password1234"
+
+# Request a username and password from the user save them as variables
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+# check if the username and password match the correct login
+if username == correctUser and password == correctPass:
+    print("Access Granted!")
+else:
+    print("Access Denied!")
