60f322625266bd2c63aaa5d427e52da187723848
Ned Perkins <nedprks@gmail.com>
2021-03-17 12:38:21 +0000

    Update guessinggame.py

--- Week-3/extra/guessinggame.py
+++ Week-3/extra/guessinggame.py
@@ -5,16 +5,26 @@ bullseye = random.randint(1,100)
 def newGame():
     bullseye = random.randint(1,100)
     winState = False
+    turnsRemain = 5
 
     while winState == False:
-        guess = input("Take a guess! ")
-        if guess == str(bullseye):
+        guess = int(input("Take a guess! "))
+
+        if guess == bullseye:
             print("You got it!")
             winState = True
-        elif guess == "x":
-            print(bullseye)
+        elif turnsRemain == 0:
+            print(f"You lost the game! The answer was... {bullseye}")
+            break
+        elif guess > bullseye:
+            print(f"You're a bit high! - {turnsRemain} turns remain.")
+            turnsRemain -= 1
+        elif guess < bullseye:
+            print(f"You're a bit low! - {turnsRemain} turns remain.")
+            turnsRemain -= 1
         else:
-            print("WRONG!")
+            print("Error!")
+            turnsRemain = 0
 
 
 newGame()
