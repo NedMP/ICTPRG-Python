ed5491d859998e0c3a2f044a7d84ea25682e5dbc
Ned Perkins <nedprks@gmail.com>
2021-03-17 11:38:17 +0000

    Food calls!

--- Week-3/extra/guessinggame.py
+++ Week-3/extra/guessinggame.py
@@ -1,3 +1,20 @@
 import random
 
-print(random.randint(0,9))
+bullseye = random.randint(1,100)
+
+def newGame():
+    bullseye = random.randint(1,100)
+    winState = False
+
+    while winState == False:
+        guess = input("Take a guess! ")
+        if guess == str(bullseye):
+            print("You got it!")
+            winState = True
+        elif guess == "x":
+            print(bullseye)
+        else:
+            print("WRONG!")
+
+
+newGame()
