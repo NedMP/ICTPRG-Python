f988259eaef5aa329799f933671d61ee8caa105a
Ned Perkins <nedprks@gmail.com>
2021-03-30 04:37:35 +0000

    import mean

--- Week-5/code/task3.py
+++ Week-5/code/task3.py
@@ -1,15 +1,5 @@
-import math
+from statistics import mean
 
 values = [89, 456, 4, 55, 232, 2, 54, 78, 65, 45, 12, 459, 35616, 45 ,78]
-subTotal = 0
-mean = 0
-maxNum = 0
 
-for i in values:
-    subTotal = subTotal + i
-    mean = math.floor(subTotal / len(values))
-    maxNum = i if i > maxNum else maxNum
-
-print(f"Sub Total: {subTotal}")
-print(f"Average: {mean}")
-print(f"Maximum Number: {maxNum}")
+print(f"Sub Total: {sum(values)}\nAverage: {mean(values)}\nMaximum Number: {max(values)}")
